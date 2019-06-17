e45056aff860430d010a0acce00726e83a5783b4
Connor Cowan <ccowan003@comcast.net>
2019-06-17 22:23:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,8 +51,8 @@ def monopoly_with_fourth_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				:reading_railroad => {},							#all these keys are set to empty hash
-				:pennsylvania_railroad => {},					#used the other syntax just because
+				:reading_railroad => {},							
+				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
 			},
